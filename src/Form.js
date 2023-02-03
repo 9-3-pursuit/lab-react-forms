df49a3748ca0b36c1d2ef758c40aeb911a07c60b
HaroldF415 <harold.f415@gmail.com>
2023-02-02 23:32:41 -0500

    mise en place

--- src/Form.js
+++ src/Form.js
@@ -1,18 +1,29 @@
 import React from "react";
 import "./Form.css";
 
+import { useState } from "react";
+
 function Form() {
+  const [values, setValues] = useState("");
+  const [result, setResult] = useState("");
+
+  const [operation, setOperation] = useState("");
+  const [validInput, setValidInput] = useState("");
+
   return (
-    <form>
-      <input id="values" name="values" type="text" />
-      <select id="operation" name="operation">
-        <option value=""></option>
-        <option value="sum">sum</option>
-        <option value="average">average</option>
-        <option value="mode">mode</option>
-      </select>
-      <button type="submit">Calculate</button>
-    </form>
+    <div className="form-container">
+      <form>
+        <input id="values" name="values" type="text" value={values} onChange={(event) => setValues(event.target.value)} className={validInput ? null : "error"} />
+        <select id="operation" name="operation" value={operation} onChange={(event) => setOperation(event.target.value)} className={validInput ? null : "error"}>
+          <option value=""></option>
+          <option value="sum">sum</option>
+          <option value="average">average</option>
+          <option value="mode">mode</option>
+        </select>
+        <button type="submit">Calculate</button>
+      </form>
+      <h1>{result}</h1>
+    </div>
   );
 }
 
