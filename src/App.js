5185890b076dec766eb6a795f52401de06746a27
AlexTan21 <87435891+AlexTan21@users.noreply.github.com>
2023-02-07 13:08:23 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,16 @@ function App() {
   return (
     <main>
       <p>Enter each number in the array, separated by a ','</p>
-      <Form />
+      <form>
+        <input id="values" name="values" type="text" />
+        <select id="operation" name="operation">
+          <option value=""></option>
+          <option value="sum">sum</option>
+          <option value="average">average</option>
+          <option value="mode">mode</option>
+        </select>
+        <button type="submit">Calculate</button>
+      </form>
       <section id="result">
         <p></p>
       </section>
