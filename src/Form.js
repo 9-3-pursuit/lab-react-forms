08f6387de469980ba1890a57e4bee75ad5e3a51d
Keeanu Rodriguez <keeanu7@gmail.com>
2023-02-07 15:08:45 -0500

    index test done

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,68 @@
 import React from "react";
 import "./Form.css";
+import { useState } from "react";
 
 function Form() {
+  const [select, setSelect] = useState("");
+  const [numberArray, setNumberArray] = useState();
+  const [result, setResult] = useState()
+
+
+  function handleSelectChange(event){
+    setSelect(event.target.value)
+  }
+  
+  
+  function handleNumberArray(event){
+    setNumberArray(event.target.value.split(",").map((num)=>{return parseInt(num)}));
+  }
+
+  function handleFormSubmit(event){
+    event.preventDefault();
+    if (select === ""){
+      setResult("Invalid input.")
+     }
+     if (select === "sum"){
+      let sum = 0;
+      numberArray.map((num)=>{ return sum+=num});
+      setResult(sum);
+     }
+     if (select === "average"){
+      let average = 0;
+      numberArray.map((num)=>{return average+=num})
+      average=average/numberArray.length;
+      setResult(average)
+     }
+     if (select === "mode"){
+      let modeObj = {};
+      let mode = numberArray[0]
+      let maxCount = 1;
+      
+      for(let i = 0; i < numberArray.length; i++){
+         let num = numberArray[i];
+  
+          if(modeObj[num] == null){
+              modeObj[num] = 1;
+          }else{
+              modeObj[num]++;
+          }  
+          if(modeObj[num] > maxCount){
+              mode = num;
+              maxCount = modeObj[num];
+          }
+      }
+      setResult(mode)
+    }
+    console.log(result)
+  }
+  
+
+
   return (
-    <form>
-      <input id="values" name="values" type="text" />
-      <select id="operation" name="operation">
+    <>
+    <form onSubmit={handleFormSubmit}>
+      <input id="values" name="values" type="text" onChange={handleNumberArray}/>
+      <select id="operation" name="operation" onChange={handleSelectChange}>
         <option value=""></option>
         <option value="sum">sum</option>
         <option value="average">average</option>
@@ -13,6 +70,10 @@ function Form() {
       </select>
       <button type="submit">Calculate</button>
     </form>
+    <section id="result">
+    <p>{result}</p>
+  </section>
+  </>
   );
 }
 
