47dd5afb271eea74d49181eed717c1f7a905c9f6
Marangely Rosas <Ladykaliber22@gmail.com>
2023-02-07 15:26:41 -0500

    Passed all tests in index.test.js file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,104 @@
 import React from "react";
-import Form from "./Form";
+//import Form from "./Form";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [numbers, setNumbers] = useState('');
+  const [type, setType] = useState('');
+  const [answer,setAnswer] = useState(null);
+
+
+function handleChange(event) {
+  const setMethod = event.target.id === 'operation' ? setType : setNumbers;
+  setMethod(event.target.value);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const mappedNumbers = numbers.split(',').map((num) => Number(num));
+
+  let textIsValid = mappedNumbers.every((num) => Number.isFinite(num));
+  let typeIsValid = type !== '';
+
+  if(!textIsValid || !typeIsValid) {
+    setAnswer('Invalid input.');
+  }
+
+  if(textIsValid && typeIsValid) {
+    if(type === 'sum') {
+      let sumAnswer = mappedNumbers.reduce((first, second) => {
+        return first + second;
+      }, 0);
+      setAnswer(sumAnswer);
+    }
+
+    if(type === 'average') {
+      let sumAnswer = mappedNumbers.reduce((first, second) => {
+        return (first + second / mappedNumbers.length);
+      }, 0);
+      setAnswer(sumAnswer);
+    }
+
+    if(type === 'mode') {
+      // count amount of occurrences of each number
+      let sumAnswer = mappedNumbers.reduce((first, second) => {
+          // create object
+          const obj = {};
+          //loop over array(mappedNumbers)
+          mappedNumbers.forEach(number => {
+            // for each number in array
+            // if it doesn't exist as a key on the object,
+            // create one and set its value to 1.
+            if (!obj[number]) {
+              obj[number] = 1;
+            } else {
+              // if it already exists as key on the object,
+              // increment its corresponding value.
+              obj[number] += 1;
+            }
+          });
+
+          //return object key with highest value.
+          let highestValue = 0;
+          let highestValueKey = -Infinity;
+
+          for (let key in obj) {
+            const value = obj[key];
+            if (value > highestValue) {
+              highestValue = value;
+              highestValueKey = key;
+            }
+          }
+        //convert key back to number 
+        return Number(highestValueKey);
+      }, 0);
+      setAnswer(sumAnswer);
+    }
+  }
+}
+
+
   return (
     <main>
       <p>Enter each number in the array, separated by a ','</p>
-      <Form />
+      <form onSubmit={handleSubmit}>
+      <input id="values" 
+      name="values" 
+      type="text" 
+      value={numbers} 
+      onChange={handleChange}
+      />
+      <select id="operation" name="operation" onChange={handleChange}>
+        <option value=""></option>
+        <option value="sum">sum</option>
+        <option value="average">average</option>
+        <option value="mode">mode</option>
+      </select>
+      <button type="submit">Calculate</button>
+    </form>
       <section id="result">
-        <p></p>
+        <p>{answer}</p>
       </section>
     </main>
   );
