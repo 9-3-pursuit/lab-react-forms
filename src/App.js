d1c1ea711bd42be3d354b81de34de9dfca9d9e79
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-02-08 09:56:12 -0500

    Adds logic to App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,70 @@
-import React from "react";
-import Form from "./Form";
+import { React, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [numbers, setNumbers] = useState("");
+  const [type, setType] = useState("");
+  const [answer, setAnswer] = useState(null);
+
+  function handleChange(e) {
+    const setMethod = e.target.id === "operation" ? setType : setNumbers;
+    setMethod(e.target.value);
+    console.log("yes i know");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const mappedNumbers = numbers.split(",").map((num) => Number(num));
+
+    let textIsValid = mappedNumbers.every((num) => Number.isFinite(num));
+
+    if (!textIsValid || type === "") {
+      setAnswer("Invalid input.");
+    }
+
+    if (type !== "mode") {
+      const sum = mappedNumbers.reduce((first, second) => {
+        return first + second;
+      }, 0);
+
+      type === "sum" ? setAnswer(sum) : setAnswer(sum / mappedNumbers.length);
+    }
+
+    if (type === "mode") {
+      let modeObj = mappedNumbers.reduce((first, second) => {
+        first.hasOwnProperty(second)
+          ? (first[second] += 1)
+          : (first[second] = 1);
+        return first;
+      }, {});
+
+      modeObj.entries();
+
+      setAnswer("");
+    }
+  }
+
   return (
     <main>
       <p>Enter each number in the array, separated by a ','</p>
-      <Form />
+      <form onSubmit={handleSubmit}>
+        <input
+          id="values"
+          name="values"
+          type="text"
+          value={numbers}
+          onChange={handleChange}
+        />
+        <select id="operation" name="operation" onChange={handleChange}>
+          <option value=""></option>
+          <option value="sum">sum</option>
+          <option value="average">average</option>
+          <option value="mode">mode</option>
+        </select>
+        <button type="submit">Calculate</button>
+      </form>
       <section id="result">
-        <p></p>
+        <p>{answer}</p>
       </section>
     </main>
   );
