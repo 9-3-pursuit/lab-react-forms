cefc87bad89a4a4536ae16ec886ee7c2da36235c
elizabethbright721 <elizabethbright721@gmail.com>
2023-02-08 16:18:07 -0500

    Forms Lab Complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,72 @@
 import React from "react";
-import Form from "./Form";
+import "./Form.css";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [input, setUserInput] = useState("")
+  let [operation, setOperation] = useState("")
+  let [result, setResult] = useState("")
+  
+  function handleUserInput(event) {
+    setUserInput(event.target.value) 
+  }
+
+  function handleOperation(event) {
+    setOperation(event.target.value)
+  }
+
+  function handleCalculation(event) {
+    event.preventDefault();
+      
+    const inputArr = input.split(",");
+    
+    if (operation === 'sum') {
+      setResult(inputArr.map((num) => Number(num)).reduce((a,b) => a + b, 0))
+    }
+
+    if (operation === 'average') {
+      setResult(inputArr.map((num) => Number(num)).reduce((a,b) => a + b, 0) / inputArr.length)
+    }
+
+    if (operation === 'mode') {
+      const numCount = {};
+      inputArr.forEach(num => { numCount[num] = (numCount[num] || 0) + 1 })
+      const valueArr = Object.values(numCount)
+      const highestCount = Math.max(...valueArr)
+      const modeIndex = valueArr.indexOf(highestCount)
+      const numMode = inputArr[modeIndex]
+
+      setResult(numMode)
+    }
+  
+    if (!input) {
+       setResult("Invalid input.")
+    }
+
+    if(!operation) {
+      setResult("Invalid input.")
+    }
+
+
+ 
+}
+
   return (
     <main>
       <p>Enter each number in the array, separated by a ','</p>
-      <Form />
+      <form>
+        <input id="values" name="values" type="text" onChange={handleUserInput} />
+        <select id="operation" name="operation" onChange={handleOperation}>
+          <option value=""></option>
+          <option value="sum">sum</option>
+          <option value="average">average</option>
+          <option value="mode">mode</option>
+        </select>
+        <button type="submit" onClick={handleCalculation}>Calculate</button>
+      </form>
       <section id="result">
-        <p></p>
+        <p>{result}</p>
       </section>
     </main>
   );
--- src/Form.js
+++ src/Form.js
@@ -1,19 +1,9 @@
 import React from "react";
 import "./Form.css";
+import { useState } from "react";
 
-function Form() {
-  return (
-    <form>
-      <input id="values" name="values" type="text" />
-      <select id="operation" name="operation">
-        <option value=""></option>
-        <option value="sum">sum</option>
-        <option value="average">average</option>
-        <option value="mode">mode</option>
-      </select>
-      <button type="submit">Calculate</button>
-    </form>
-  );
+
+export default function Form() {
+return;
+
 }
-
-export default Form;
