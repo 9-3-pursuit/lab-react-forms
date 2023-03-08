80258c0d22cf28c9b7ec895275ce0e9d20ea4e36
wal3sca <walescaambroise@gmail.com>
2023-03-08 11:29:27 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React from "react";
 import Form from "./Form";
 import "./App.css";
 
-function App() {
-  return (
-    <main>
-      <p>Enter each number in the array, separated by a ','</p>
-      <Form />
-    </main>
-  );
+// to change userInput you can use setState OR use this.setState.userInput.value
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      result: "",
+    };
+  }
+
+  render() {
+    return (
+      <main>
+        <p>Enter each number in the array, separated by a ','</p>
+        <Form fun={this} />
+        <section id="result">
+          <p>{this.state.result}</p>
+        </section>
+      </main>
+    );
+  }
 }
 
 export default App;
