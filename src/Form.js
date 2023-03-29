6a1870b4c5c03a557e6cdaaff95bdbad6bff9162
riyaalexander <riyaalexander@Riyas-MacBook-Air.local>
2023-03-29 17:43:59 -0400

    forms

--- src/Form.js
+++ src/Form.js
@@ -1,12 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 
 function Form() {
+  const [values, setValues] = useState("");
+  const [operation, setOperation] = useState("");
+  const [result, setResult] = useState("");
+  const [hasError, setHasError] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Split input into array of numbers
+    const valuesArray = values.split(",").map((value) => Number(value.trim()));
+
+    // Validate input
+    if (valuesArray.every((value) => !isNaN(value)) && operation !== "") {
+      let calculatedResult;
+      // Calculate result based on operation
+      switch (operation) {
+        case "sum":
+          calculatedResult = valuesArray.reduce((acc, cur) => acc + cur, 0);
+          break;
+        case "average":
+          calculatedResult = valuesArray.reduce((acc, cur) => acc + cur, 0) / valuesArray.length;
+          break;
+        case "mode":
+          const frequency = valuesArray.reduce((acc, cur) => {
+            acc[cur] = (acc[cur] || 0) + 1;
+            return acc;
+          }, {});
+          let maxFrequency = 0;
+          for (const key in frequency) {
+            if (frequency[key] > maxFrequency) {
+              calculatedResult = key;
+              maxFrequency = frequency[key];
+            }
+          }
+          break;
+        default:
+          break;
+      }
+      setResult(calculatedResult);
+      // Clear input fields
+      setValues("");
+      setOperation("");
+      setHasError(false);
+    } else {
+      setResult("Invalid input.");
+      setHasError(true);
+    }
+  };
+
   return (
     <div>
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+      <form onSubmit={handleSubmit}>
+        <input id="values" name="values" type="text" className={hasError ? "error" : ""} value={values} onChange={(event) => setValues(event.target.value)} />
+        <select id="operation" name="operation" className={hasError ? "error" : ""} value={operation} onChange={(event) => setOperation(event.target.value)}>
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
@@ -15,7 +64,7 @@ function Form() {
         <button type="submit">Calculate</button>
       </form>
       <section id="result">
-        <p></p>
+        <p>{result}</p>
       </section>
     </div>
   );
